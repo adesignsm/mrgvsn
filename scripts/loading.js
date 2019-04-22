fcf9ffde1cb15dbc6e1256012466c2d14db0c5d2
Akash Mulye <akashmulye97@gmial.com>
2019-04-22 18:22:34 -0400

    third commit

--- scripts/loading.js
+++ scripts/loading.js
@@ -11,3 +11,55 @@ var randomQuote = loadingQuoteArr[Math.floor(Math.random() * loadingQuoteArr.len
 
 var loadingPageQuote = document.getElementById("quote-generator");
 loadingPageQuote.innerHTML = randomQuote;
+
+function loading_canvas() {
+
+	var scene = new THREE.Scene();
+	var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+	var renderer = new THREE.WebGLRenderer({antialias: true});
+	renderer.shadowMapEnabled = true;
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+	document.getElementById("loading-page").appendChild(renderer.domElement);
+
+	window.addEventListener("resize", function() {
+
+		var width = window.innerWidth;
+		var height = window.innerHeight;
+
+		renderer.setSize(width, height);
+
+		camera.aspect = width / height;
+		camera.updateProjectionMatrix();
+	});
+
+	camera.position.z = 20;
+
+	var loadingLogoGeo = new THREE.PlaneGeometry(8, 7, 1);
+	var loadingLogoMaterial = new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("./assets/bfly.png"), side: THREE.DoubleSide});
+	var loadingLogo = new THREE.Mesh(loadingLogoGeo, loadingLogoMaterial);
+	scene.add(loadingLogo);
+
+	var update = function() {
+
+		loadingLogo.rotation.y += 0.02;
+	};
+
+	var render = function() {
+
+		renderer.render(scene, camera);
+	};
+
+	var animate = function() {
+
+		requestAnimationFrame(animate);
+
+		update();
+		render();
+	};
+
+	animate();
+}
+
+loading_canvas();
